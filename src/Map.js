d3fa905112dc4a0f198fb86dc01f38601d565ade
Ezequiel Pintos <ezequiel@42mate.com>
2017-07-28 21:35:14 -0300

    Moving NewPoint component to App component.

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import NewPoint from './NewPoint';
 
 class Map extends Component {
   render() {
     return (
       <div>
         Mapa!
-        <NewPoint onNewPoint={this.props.onNewPoint} />
       </div>
     );
   }
